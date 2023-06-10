6ddff11c830a52b666a877d2c34dc9586c64e5c6
purna <purnaraomba@gmail.com>
2023-06-10 15:54:50 +0530

    RoboShop Update

--- common.sh
+++ common.sh
@@ -82,13 +82,14 @@ rm -rf /app/* &>>${log_file}
 status_check $?
 
 print_head "Downloading App Content"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component} &>>${log_file}
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
 status_check $?
 cd /app
 
-print_head "Extracting App content"
+print_head "Extracting App Content"
 unzip /tmp/${component}.zip &>>${log_file}
 status_check $?
+
 }
 
 nodejs() {
@@ -107,9 +108,11 @@ nodejs() {
   npm install &>>${log_file}
   status_check $?
 
+  systemd_setup
+
   schema_setup
 
-  systemd_setup
+
 
 }
 
