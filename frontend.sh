20030c8200bae1b5af24e19c8c1127be48365279
purna <purnaraomba@gmail.com>
2023-10-15 10:44:33 +0530

    Roboshop Update

--- frontend.sh
+++ frontend.sh
@@ -18,7 +18,7 @@ unzip /tmp/frontend.zip &>>${log_file}
 status_check $?
 
 print_head "Copying Nginx Config for RoboShop"
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+cp /configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 status_check $?
 
 print_head "Enabling nginx"
