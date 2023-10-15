eb2b6d2357ca98318fed3efb33d290ee43a49870
purna <purnaraomba@gmail.com>
2023-10-15 10:38:18 +0530

    Roboshop Update

--- ec2-servers.sh
+++ ec2-servers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##### Change these values ###
-ZONE_ID="Z103214126L48SQW30RSR"
-DOMAIN="devopsb71.online"
+ZONE_ID="Z01280802SKTCPOFIGWX3"
+DOMAIN="roboz.online"
 SG_NAME="allow-all"
 #############################
 
