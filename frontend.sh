547529d0acd4dedc29886b2327ef97c44bd1268e
purna <purnaraomba@gmail.com>
2023-10-15 09:39:59 +0530

    Roboshop Update

---
+++ frontend.sh
@@ -0,0 +1,30 @@
+source common.sh
+
+print_head "Installing nginx"
+yum install nginx -y &>>${log_file}
+status_check $?
+
+print_head "Removing Old Content"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
+
+print_head "Downloading Frontend Content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
+
+print_head "Extracting Downloaded Frontend"
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
+
+print_head "Copying Nginx Config for RoboShop"
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
+
+print_head "Enabling nginx"
+systemctl enable nginx &>>${log_file}
+status_check $?
+
+print_head "Starting nginx"
+systemctl restart nginx &>>${log_file}
+status_check $?
