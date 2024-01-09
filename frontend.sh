b749d0bd7947db7db3c69f3c18988b194367f427
purna <purnaraomba@gmail.com>
2024-01-09 17:46:31 +0530

    Roboshop Update

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
 source common.sh
 
-print_head "Installing nginx"
+print_head "Installing Nginx"
 yum install nginx -y &>>${log_file}
 status_check $?
 
@@ -9,11 +9,12 @@ rm -rf /usr/share/nginx/html/* &>>${log_file}
 status_check $?
 
 print_head "Downloading Frontend Content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 status_check $?
 
-print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html
+
+print_head "Extracting Downloaded Frontend"
 unzip /tmp/frontend.zip &>>${log_file}
 status_check $?
 
@@ -21,14 +22,10 @@ print_head "Copying Nginx Config for RoboShop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 status_check $?
 
-print_head "Enabling nginx"
+print_head "Enabling Nginx"
 systemctl enable nginx &>>${log_file}
 status_check $?
 
-print_head "Starting nginx"
-systemctl restart nginx &>>${log_file}
+print_head "Starting Nginx"
+systemctl start nginx
 status_check $?
-
-
-## If any command is errored or failed, we need to stop the script
-# Status of a command need to be printed.
